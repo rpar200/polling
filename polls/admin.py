3c53790a6627455f9698e2b2bc770729944c9c2b
Rufus <rufus.park1@gmail.com>
2024-08-27 13:35:34 -0400

    Added Question Model to admin page and added __str__ method to Question and Choice models for easier terminal readability.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+## We're telling django that Question objects have an admin interface!
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,21 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    ## important to add __str__ methods to the models.
+    ## This helps make the terminal more readable. We can see the actual text, instead of the object
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
